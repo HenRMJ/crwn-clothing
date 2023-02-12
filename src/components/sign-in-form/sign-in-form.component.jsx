947f90f7f63da663b43cf7fd732b380feaa1fdaa
Henry <65220433+HenRMJ@users.noreply.github.com>
2023-02-12 11:31:51 -0800

    Refactor authentication
    I refactored the way that authentication worked so that the user is saved in a current user state inside of a context. So it can persist throughout the site. I also added a small functionality with the navbar where a user can sign out once they've signed in.

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { signInWithGooglePopup, createUserDocumentFromAuth, signInAuthUserWithEmailAndPassword } from '../../utils/firebase/firebase.utils';
+
 import FormInput from '../form-input/form-input.component';
 import Button from '../button/button.component';
 
@@ -14,23 +15,19 @@ const SignInForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { email, password } = formFields;
 
-    console.log(formFields);
-
     const resetFormFields = () => {
         setFormFields(defaultFormFields);
     }
 
     const signInWithGoogle = async () => {
-        const { user } = await signInWithGooglePopup();
-        await createUserDocumentFromAuth(user);
+        await signInWithGooglePopup();
     }
 
     const handleSubmit = async (event) => {
         event.preventDefault();
 
         try {
-            const response = await signInAuthUserWithEmailAndPassword(email, password);
-            console.log(response);
+            await signInAuthUserWithEmailAndPassword(email, password);
 
             resetFormFields();
         } catch (error) {
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -30,10 +30,7 @@ const firebaseConfig = {
     if (!userAuth) return;
 
     const userDocRef = doc(db, 'users', userAuth.uid);
-    console.log(userDocRef);
-
     const userSnapshot = await getDoc(userDocRef);
-    console.log(userSnapshot.exists());
 
     if (!userSnapshot.exists()) {
         const { displayName, email } = userAuth;
@@ -64,3 +61,6 @@ const firebaseConfig = {
 
     return await signInWithEmailAndPassword(auth, email, password);
   }
+
+  export const signOutUser = async () => await signOut(auth);
+  export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback);
