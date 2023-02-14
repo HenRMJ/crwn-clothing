e8964ed2076d32fb59b808fc695cfba21d5e6031
Henry <65220433+HenRMJ@users.noreply.github.com>
2023-02-13 16:26:43 -0800

    Separate sample code
    I removed everything besides the sample code from this branch.

--- sign-up-form.component.jsx
+++ sign-up-form.component.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
+
+// Created utility functions to make it easier to refactor code in the case of future changes to firebase code.
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 import FormInput from '../form-input/form-input.component';
 import Button from '../button/button.component';
 
 import './sign-up-form.styles.scss';
 
+// default input for the state hook and is used to resset the form fields after a successful sign up
 const defaultFormFields = {
     displayName: '',
     email: '',
@@ -13,7 +16,10 @@ const defaultFormFields = {
 };
 
 const SignUpForm = () => {
+    // Hook to keep track of the different fields in one object
     const [formFields, setFormFields] = useState(defaultFormFields);
+
+    // destructured values for easier access to each field in the formFields object
     const { displayName, email, password, confirmPassword } = formFields;
 
     const resetFormFields = () => {
@@ -24,18 +30,21 @@ const SignUpForm = () => {
         event.preventDefault();
 
         if (password !== confirmPassword) {
-            alert("Passwrods do not match");
+            alert("Passwords do not match");
             return;
         }
 
         try {
-            const { user } = await createAuthUserWithEmailAndPassword(email, password);            
-            await createUserDocumentFromAuth(user, { displayName });
+            // Creates the authenticated account in firebase
+            const { user } = await createAuthUserWithEmailAndPassword(email, password);      
             
+            // Creates the user in the firestore database with the display name from the form field
+            await createUserDocumentFromAuth(user, { displayName });
+
             resetFormFields();
         } catch (error) {
             if (error.code === 'auth/email-already-in-use') {
-                alert('Cannot create account, email already in use')
+                alert('Cannot create account, email already in use');
             } else {
                 console.log('user creation encountered an error ' + error.message);
             }
@@ -45,10 +54,12 @@ const SignUpForm = () => {
     const handleChange = event => {
         const { name, value } = event.target;
 
-        setFormFields({...formFields, [name]: value})
+        // sets the selected key taken from the name of the input field with the value from the input field
+        setFormFields({...formFields, [name]: value});
     }
 
     return (
+        // Sign up form
         <div className="sign-up-container">
             <h2>Don't Have an Account?</h2>
             <span>Sign up with your email and password</span>
