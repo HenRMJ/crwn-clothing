201f5232ba21abab023c3207351141ed1ebbea4c
Henry <65220433+HenRMJ@users.noreply.github.com>
2023-02-14 14:53:11 -0800

    Add Cart and Shop page
    I added in the cart icon and created a context for it. I also created the styles and component for the cart. So when you click the cart will open but no other functionality beyond that. I also added in the hats shopping page with a local json file. This will be exported out in the future.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from "./routes/home/home.component";
 import Authentication from './routes/authentication/authentication.component';
 import Navigation from './routes/navigation/navigation.component';
-
-const Shop = () => {
-  return (
-    <div>
-      <h1>This is the shop</h1>
-    </div>
-  );
-}
+import Shop from './routes/shop/shop.component';
 
 const App = () => {
   return (
